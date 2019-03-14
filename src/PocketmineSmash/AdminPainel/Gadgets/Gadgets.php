57ecc8ef7169b896ee3a28f8d8a8d982d61d0f6e
PocketmineSmash <diego23gonza@hotmail.com>
2019-03-13 19:56:06 -0500

    Fixed kick

--- src/PocketmineSmash/AdminPainel/Gadgets/Gadgets.php
+++ src/PocketmineSmash/AdminPainel/Gadgets/Gadgets.php
@@ -1,37 +1,32 @@
 <?php
 declare(strict_types=1);
-
 namespace PocketmineSmash\AdminPainel\Gadgets;
-
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
 use PocketmineSmash\AdminPainel\Main;
 use pocketmine\Player;
 use pocketmine\inventory\PlayerInventory;
 use pocketmine\item\Item;
 use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\level\Level;
 
 class Gadgets implements Listener {
     public function __construct(Main $main)
     {
         $this->main = $main;
     }
-
-
-    public function onKick(EntityDamageByEntityEvent $event, Player $player, PlayerInteractEvent $e) {
+    public function onKick(EntityDamageEvent $event)
+    {
         if ($event instanceof EntityDamageByEntityEvent) {
             $admin = $event->getDamager();
             $user = $event->getEntity();
-            if($admin instanceof Player and $user instanceof Player) {
-                if($admin->getInventory()->getItemInHand()->getId() == 280){
-                    $user->getPlayer()->kick("Kicked FromServer");
-                }
+            if ($admin instanceof Player and $user instanceof Player) {
+                if ($admin->getInventory()->getItemInHand()->getId() == 280) {
+                    $user->kick("Kicked");
                 }
             }
         }
+    }
+
 }
-
-
-
-
